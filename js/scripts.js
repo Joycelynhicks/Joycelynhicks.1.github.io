298f018ccb310d5f88a3b4cd6b36edd12a655e88
Joycelyn Hicks <joyhicks23@gmail.com>
2020-08-21 20:16:10 -0600

    added positioning to class

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    //$("#result_msg").hide();
+    //$("#button").click(function(e) {
+        //$('.container').addclass('anim')
+        //$('#result_msg').fadeIn(1500)
+        //e.preventDefault ();
+       // searchingstring = $('#searchingfield'.value();
+       // $('.results *').fadeOut(300, function () $(this).remove();
+
+    //$.getJSON(
+   //     {
+        //    (url:'api.giphy.com/v1/gifs/search?api_key=lR1UUS4tJNxsOi7rD9s7EJuiVS2hEINo');
+     //   }
+  //  )
+
+  //  }
+});
